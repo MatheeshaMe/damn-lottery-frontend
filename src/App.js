a717048ddc62068554a559b750c8c879c66f9794
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-10-16 06:49:39 +0530

    connect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
+import { ethers } from "ethers";
 import React, { useState, useEffect } from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Timer from "./components/timer/Timer";
-
+import { abi, lotteryContractAddress } from "./config";
+import { provier } from "./services/provider";
 function App() {
-  return <div className="container">
-    <Navbar/>
-    <Timer/>
-    <Hero/>
-  </div>;
+  const [lotteryContract, setLotteryContract] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+
+  const connectToMetamask = async () => {
+    const { ethereum } = window;
+    if (ethereum.isMetaMask) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      const { name, chainId } = await provider.getNetwork();
+      const lotteryContract = new ethers.Contract(
+        lotteryContractAddress,
+        abi,
+        provider
+      );
+      setLotteryContract(lotteryContract);
+      console.log(lotteryContract.address);
+      setAccount(accounts[0]);
+    } else {
+      console.log("install metamask");
+    }
+  };
+
+  const enterToLotteryGame = () => {
+    console.log("enter");
+  };
+
+  const winner = () => {
+    console.log("winner");
+  };
+
+  useEffect(() => {
+    winner();
+  }, [account]);
+
+  return (
+    <div className="container">
+      <Navbar account={account} connectToMetamask={connectToMetamask} />
+      <Timer />
+      <Hero
+      account={account}
+        connectToMetamask={connectToMetamask}
+        enterToLotteryGame={enterToLotteryGame}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+import abis from "../src/abis/Lottery.sol/Lottery.json"
+
+export const lotteryContractAddress="0x1a6B16718Fb84e031F553DF20F05b9CB3A7e7e5e"
+export const network= "alchemy"
+export const networkId = 5
+export const abi = abis.abi
+export const alchemyWebScoketUrl = process.env.ALCHEMY_MATICWSC_URL;
+export const alchemyMaticUrl = process.env.LCHEMY_MATIC_URL;
